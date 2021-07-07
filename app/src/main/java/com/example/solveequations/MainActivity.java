27d51fd26db718597d722410371b0f32efa79568
ExpertSpace <02mago95@gmail.com>
2021-07-07 21:04:39 +0300

    saved working version

--- app/src/main/java/com/example/solveequations/MainActivity.java
+++ app/src/main/java/com/example/solveequations/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupMathView();
-        randomCoefficientQuadraticEquations();
+        setMathViewRandomCoefficientQuadraticEquations();
     }
 
     private void setupMathView() {
@@ -34,40 +34,143 @@ public class MainActivity extends AppCompatActivity {
         mathView.setTextColor("#3ecd5e");
     }
 
-    private void randomCoefficientQuadraticEquations() {
-        a = (int) (0 + Math.random() * 5);
-        b = (int) (0 + Math.random() * 5);
-        c = (int) (0 + Math.random() * 5);
+    private void setMathViewRandomCoefficientQuadraticEquations() {
+        a = (int) (-5 + Math.random() * 5);
+        b = (int) (-5 + Math.random() * 5);
+        c = (int) (-5 + Math.random() * 5);
 
-        if (a == 0 && b == 0 && c == 0) {
+//        a = 0;
+//        b = 0;
+//        c = 0;
+
+        String outputResult = "";
+
+        // 1
+        if ((a == 0 && b == 0 && c == 0)) {
             a = (int) (8 + Math.random() * 10);
             b = (int) (4 + Math.random() * 7);
             c = (int) (1 + Math.random() * 3);
+
+            outputResult = a + "x^2 + " + b + "x + " + c + " = 0";
+
+            Log.d("QQQ", "======================= VALUES 1 START =======================");
+            Log.d("QQQ", "A = " + a);
+            Log.d("QQQ", "B = " + b);
+            Log.d("QQQ", "C = " + c);
+            Log.d("QQQ", "VALUES = 1       ===       " + outputResult);
+            Log.d("QQQ", "======================= VALUES 1 END =========================");
         }
 
-        String outputResult = a + "x^2 + " + b + "x + " + c + " = 0";
-        if (a == 0) {
-            outputResult = outputResult.replaceAll("\\dx\\^2\\W+", "");
-        Log.d("QQQ: ", outputResult);
+        // 2
+        else if (a == 0 && b == 0 && c != 0) {
+            a = (int) (-8 + Math.random() * 10);
+            b = (int) (-4 + Math.random() * 7);
+
+            if (c < 0)
+                outputResult = a + "x^2 + " + b + "x " + c + " = 0";
+            else
+                outputResult = a + "x^2 + " + b + "x + " + c + " = 0";
+
+            Log.d("QQQ", "======================= VALUES 2 START =======================");
+            Log.d("QQQ", "A = " + a);
+            Log.d("QQQ", "B = " + b);
+            Log.d("QQQ", "C = " + c);
+            Log.d("QQQ", "VALUES = 1       ===       " + outputResult);
+            Log.d("QQQ", "======================= VALUES 2 END =========================");
         }
-        if (b == 0) {
-            outputResult = outputResult.replaceAll("(\\s*\\dx*)x\\s\\W+", "");
-            Log.d("QQQ: ", outputResult);
+
+        // 3
+        else if (a == 0 && b != 0 && c == 0) {
+            outputResult = b + "x" + " = 0";
+
+            Log.d("QQQ", "======================= VALUES 3 START =======================");
+            Log.d("QQQ", "A = " + a);
+            Log.d("QQQ", "B = " + b);
+            Log.d("QQQ", "C = " + c);
+            Log.d("QQQ", "VALUES = 1       ===       " + outputResult);
+            Log.d("QQQ", "======================= VALUES 3 END =========================");
         }
-        if (c == 0)
-            outputResult = a + "x^2 + " + b + "x" + " = 0" ;
-        if (c == 1)
-            outputResult = a + "x^2 + " + b + "x + " + c + " = 0";
-        if (outputResult.contains("1")) {
-            outputResult = outputResult.replaceFirst("1", "");
-            if(outputResult.contains("1"))
-                outputResult = outputResult.replaceFirst("1", "");
+
+        // 4
+        else if (a == 0 && b != 0 && c != 0) {
+            if (c < 0)
+                outputResult = b + "x " + c + " = 0";
+            else
+                outputResult = b + "x + " + c + " = 0";
+
+            Log.d("QQQ", "======================= VALUES 4 START =======================");
+            Log.d("QQQ", "A = " + a);
+            Log.d("QQQ", "B = " + b);
+            Log.d("QQQ", "C = " + c);
+            Log.d("QQQ", "VALUES = 1       ===       " + outputResult);
+            Log.d("QQQ", "======================= VALUES 4 END =========================");
+        }
+
+        // 5
+        else if (a != 0 && b == 0 && c == 0) {
+            outputResult = a + "x^2" + " = 0";
+
+            Log.d("QQQ", "======================= VALUES 5 START =======================");
+            Log.d("QQQ", "A = " + a);
+            Log.d("QQQ", "B = " + b);
+            Log.d("QQQ", "C = " + c);
+            Log.d("QQQ", "VALUES = 1       ===       " + outputResult);
+            Log.d("QQQ", "======================= VALUES 5 END =========================");
+        }
+
+        // 6
+        else if (a != 0 && b == 0 && c != 0) {
+            if (c < 0)
+                outputResult = a + "x^2 " + c + " = 0";
+            else
+                outputResult = a + "x^2 + " + c + " = 0";
+
+            Log.d("QQQ", "======================= VALUES 6 START =======================");
+            Log.d("QQQ", "A = " + a);
+            Log.d("QQQ", "B = " + b);
+            Log.d("QQQ", "C = " + c);
+            Log.d("QQQ", "VALUES = 1       ===       " + outputResult);
+            Log.d("QQQ", "======================= VALUES 6 END =========================");
+        }
+
+        // 7
+        else if (a != 0 && b != 0 && c == 0) {
+            if (b < 0)
+                outputResult = a + "x^2 " + b + "x" + " = 0";
+            else
+                outputResult = a + "x^2 + " + b + "x" + " = 0";
+
+            Log.d("QQQ", "======================= VALUES 7 START =======================");
+            Log.d("QQQ", "A = " + a);
+            Log.d("QQQ", "B = " + b);
+            Log.d("QQQ", "C = " + c);
+            Log.d("QQQ", "VALUES = 1       ===       " + outputResult);
+            Log.d("QQQ", "======================= VALUES 7 END =========================");
+        }
+
+        // 8
+        else {
+            if (b < 0 && c > 0)
+                outputResult = a + "x^2 " + b + "x + " + c + " = 0";
+            else if (b > 0 && c < 0)
+                outputResult = a + "x^2 + " + b + "x " + c + " = 0";
+            else if (b < 0 && c < 0)
+                outputResult = a + "x^2 " + b + "x " + c + " = 0";
+            else
+                outputResult = a + "x^2 + " + b + "x + " + c + " = 0";
+
+            Log.d("QQQ", "======================= VALUES 8 START =======================");
+            Log.d("QQQ", "A = " + a);
+            Log.d("QQQ", "B = " + b);
+            Log.d("QQQ", "C = " + c);
+            Log.d("QQQ", "VALUES = 1       ===       " + outputResult);
+            Log.d("QQQ", "======================= VALUES 8 END =========================");
         }
 
         mathView.setText(MATH_ML_INSERT + outputResult + MATH_ML_INSERT);
     }
 
     public void updateClick(View view) {
-        randomCoefficientQuadraticEquations();
+        setMathViewRandomCoefficientQuadraticEquations();
     }
 }
